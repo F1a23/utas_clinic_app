fb3c27bc8d2f7036f42538a4db9fad21cd52da9a
fatima <fatima5alamri8@gmail.com>
2025-05-14 15:06:34 +0400

    ok3

--- server/index.js
+++ server/index.js
@@ -20,7 +20,13 @@ import ContactMode from "./Models/ContactMode.js";
 import WebinarRegistrationModel from "./Models/WebinarRegistrationModel.js";
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://utas-clinic-app-client.onrender.com", // اسم موقع الواجهة
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 // Database connection
 // Database connection
