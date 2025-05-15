f6bdaea52f03061d8eb0da7bafa7d84257d9a2dd
fatima <fatima5alamri8@gmail.com>
2025-05-15 09:14:31 +0400

    ok39

--- server/index.js
+++ server/index.js
@@ -1441,9 +1441,6 @@ app.get("/getAllMedicationRequests", async (req, res) => {
 
 // هذا الـ catch-all route لحل مشكلة التحديث
 app.use(express.static(path.join(__dirname, "client", "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
 
 const port = ENV.PORT || 3001;
 
